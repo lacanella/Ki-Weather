396bcda509e9ee78a94345d9771acc081be6b9d7
lacanella <107126483+lacanella@users.noreply.github.com>
2022-08-07 16:57:36 +0300

    to add humidity and wind value

--- src/7java.js
+++ src/7java.js
@@ -1,3 +1,34 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours<10){hours=`0${hours}`;}
+
+  let minutes = date.getMinutes();
+  if minutes (minutes<10){minutes=`0${minutes}`;}
+
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days [date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function ShowDay (timestamp) {
+  let date = new Date(timestamp*1000);
+  let day = date.getDay();
+  let days = ["Sun","Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+  return days[day]
+}
+
+  let dateElement = document.querySelector("currentdate");
+  dateElement.innerHTML = formatDate(timestamp);
+
 function showTemperature(responce) {
   let tempElement = document.querySelector("#temperature");
   tempElement.innerHTML = Math.round(responce.data.main.temp);
@@ -8,14 +39,14 @@ function showTemperature(responce) {
   let conditionElement = document.querySelector("#conditions");
   conditionElement.innerHTML = responce.data.weather[0].description;
 
-  let precipitationElement = document.querySelector("#precipitation");
-  precipitationElement.innerHTML = responce.data.precipitation;
+ 
 
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = responce.data.humidity;
+  humidityElement.innerHTML = responce.data.main.humidity;
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(responce.data.wind.speed);
+
 }
 
 let apiKey = "fc91beb744f93e422747179ad98c56f9";
